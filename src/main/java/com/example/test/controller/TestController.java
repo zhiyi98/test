afcc42e54f62f354b910c3042cd75fa09a451b98
zhiyi98 <yuanzhiyi98@gmail.com>
2022-10-20 21:30:15 +0800

    feat:build project

---
+++ src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @GetMapping("/test1")
+    public String test1(){
+        return "hello";
+    }
+    
+}
